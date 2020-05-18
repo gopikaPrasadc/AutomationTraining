fd03b995dfb04e6a920573826c4dfcdd280ae513
user <user@DESKTOP-E5EEI77>
2020-05-18 11:28:49 +0530

    comment

--- PayRoll/src/test/java/utilities/BaseClass.java
+++ PayRoll/src/test/java/utilities/BaseClass.java
@@ -1,6 +1,7 @@
 package utilities;
 
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -11,6 +12,7 @@ public class BaseClass {
   public void beforeTest() {
 	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\user\\Desktop\\Gopika\\drivers\\chromedriver.exe");
   driver = new ChromeDriver();
+  driver.manage().window().setSize(new Dimension(1600, 1200));
   driver.get("https://www.qabible.in/payrollapp/site/login");
   }
 
